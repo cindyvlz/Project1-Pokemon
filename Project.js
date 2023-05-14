91098dc2f64e7fbaa0e63dd029daacc53e7f96cd
Cindy Velasquez <ciindvlz@hotmail.com>
2023-05-14 17:30:00 -0400

    create a function to get the details from our pokemon

--- Project.js
+++ Project.js
@@ -1,3 +1,12 @@
 const pokemonList = document.querySelector('.pokemonsList');
 const searchInputs = document.getElementById('searchInput');
 const searchForm = document.getElementById('searchForm');
+
+//getting details from the pokemon to add to our card
+function gettingDetails(pokemonDetails,pokemonurl){
+    console.log("Getting details for:", pokemonurl);
+      fetch(pokemonurl)
+          .then(res => res.json())
+          .then(data => {
+              const pokemonAbilities = data.abilities
+              const pokemonMove = data.moves.slice(0, 3)
